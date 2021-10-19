5d8a94d65eb8031dbd787039026796b5550c431b
Jose Guerra <jose.guerra@dodsgroup.com>
2021-10-19 10:38:46 +0100

    Converted code to component

--- src/sandbox-section-3/visibleToggle.js
+++ src/sandbox-section-3/visibleToggle.js
@@ -1,27 +1,59 @@
-const appRoot = document.getElementById('app')
-let visibility = false
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleToggle = this.handleToggle.bind(this)
+    this.state = {
+      visibility: false,
+      title: 'Visibility Toggle',
+      details: 'Put your life in the hands of a computer'
+    }
+  }
 
+  handleToggle() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility
+      }
+    })
+  }
 
-const app = {
-  title: 'Visibility Toggle',
-  details: 'Put your life in the hands of a computer'
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <button onClick={this.handleToggle}>{this.state.visibility ? 'Hide details' : 'Show details'}</button>
+        {this.state.visibility && (<p>{this.state.details}</p>)}
+      </div>
+    )
+  }
 }
 
-const toggleDetails = () => {
-  visibility = !visibility
-  renderApp()
-}
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'))
 
-const renderApp = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      <button onClick={toggleDetails}>{visibility ? 'Hide details' : 'Show details'}</button>
-      {visibility && (<p>{app.details}</p>)}
-    </div>
-  )
+// const appRoot = document.getElementById('app')
+// let visibility = false
 
-  ReactDOM.render(template, appRoot)
-}
 
-renderApp()
+// const app = {
+//   title: 'Visibility Toggle',
+//   details: 'Put your life in the hands of a computer'
+// }
+
+// const toggleDetails = () => {
+//   visibility = !visibility
+//   renderApp()
+// }
+
+// const renderApp = () => {
+//   const template = (
+//     <div>
+//       <h1>{app.title}</h1>
+//       <button onClick={toggleDetails}>{visibility ? 'Hide details' : 'Show details'}</button>
+//       {visibility && (<p>{app.details}</p>)}
+//     </div>
+//   )
+
+//   ReactDOM.render(template, appRoot)
+// }
+
+// renderApp()
