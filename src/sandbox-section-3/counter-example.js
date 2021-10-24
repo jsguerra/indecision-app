769dabe695febefd802fbcd16789e1a0d8417092
jsguerra <jose@gcostudios.com>
2021-10-24 20:10:31 +0100

    default props added

--- src/sandbox-section-3/counter-example.js
+++ src/sandbox-section-3/counter-example.js
@@ -5,7 +5,7 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this)
     this.handleReset = this.handleReset.bind(this)
     this.state = {
-      count: 0
+      count: props.count
     }
   }
 
@@ -42,6 +42,55 @@ class Counter extends React.Component {
     )
   }
 }
+
+Counter.defaulProps = {
+  count: 0
+}
+
+// class Counter extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.handleAddOne = this.handleAddOne.bind(this)
+//     this.handleMinusOne = this.handleMinusOne.bind(this)
+//     this.handleReset = this.handleReset.bind(this)
+//     this.state = {
+//       count: 0
+//     }
+//   }
+
+//   handleAddOne() {
+//     this.setState((prevState) => {
+//       return {
+//         count: prevState.count + 1
+//       }
+//     })
+//   }
+//   handleMinusOne() {
+//     this.setState((prevState) => {
+//       return {
+//         count: prevState.count - 1
+//       }
+//     })
+//   }
+//   handleReset() {
+//     this.setState(() => {
+//       return {
+//         count: 0
+//       }
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Count: {this.state.count}</h1>
+//         <button onClick={this.handleAddOne}>+1</button>
+//         <button onClick={this.handleMinusOne}>-1</button>
+//         <button onClick={this.handleReset}>reset</button>
+//       </div>
+//     )
+//   }
+// }
 
 ReactDOM.render(<Counter />, document.getElementById('app'))
 
